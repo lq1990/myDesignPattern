da94cc8b7c2999e3d838fcdc557c03aeebee93ce
Qiang <chinalq90@gmail.com>
2018-12-23 21:20:16 +0100

    first

---
+++ TestClass.js
@@ -0,0 +1,54 @@
+/**
+* Generated On: 2018-12-23
+* Class: TestClass
+*/
+
+function TestClass(){
+    //Constructor
+
+    this.pubAttr = null;
+    this.protAttr = null;
+    this.privAttr = null;
+    this.pkgAttr = null;
+
+}
+
+
+/**
+* @return {null}
+*/
+TestClass.prototype.pubMethod = function(){
+    //TODO: Implement Me 
+
+};
+
+
+/**
+* @return {null}
+*/
+TestClass.prototype.protMethod = function(){
+    //TODO: Implement Me 
+
+};
+
+
+/**
+* @return {null}
+*/
+TestClass.prototype.privMethod = function(){
+    //TODO: Implement Me 
+
+};
+
+
+/**
+* @return {null}
+*/
+TestClass.prototype.pkgMethod = function(){
+    //TODO: Implement Me 
+
+};
+
+
+
+module.exports = {TestClass:TestClass};

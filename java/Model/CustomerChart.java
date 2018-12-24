9e0ad3afa996ac55737e76a176575c3b96cca0f0
lqLinux <chinalq90@gmail.com>
2018-12-24 17:15:32 +0100

    srp, ocp

---
+++ java/Model/CustomerChart.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class CustomerChart {
+
+    /**
+     * Default constructor
+     */
+    public CustomerChart() {
+    }
+
+    /**
+     * 
+     */
+    public CustomerDao customerDao;
+
+
+    /**
+     * 
+     */
+    public void displayChart() {
+        // TODO implement here
+    }
+
+}
---
+++ java/Model/CustomerDao.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class CustomerDao {
+
+    /**
+     * Default constructor
+     */
+    public CustomerDao() {
+    }
+
+
+    /**
+     * 
+     */
+    public void findCustomers() {
+        // TODO implement here
+    }
+
+}
